d14413b2db40bb0d07ccc94c4d3454e97b5d29cd
Piyush Khanna <khanna.piyush30@gmail.com>
2021-05-19 17:30:38 +0530

    basic setup and component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const name = "Piyush";
+  const x = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello From React</h1>
+        <h2>Hello {name}</h2>
+        <h3>Hello {x?'Yes':'No'}</h3>
+        <Header/>
     </div>
   );
 }
