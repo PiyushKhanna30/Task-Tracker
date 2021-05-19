b317ce30acd37c02c6e5b1ea42fd3f0a1973ba9b
Piyush Khanna <khanna.piyush30@gmail.com>
2021-05-19 17:31:08 +0530

    clean

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import './App.css';
 import Header from './components/Header';
 
 function App() {
-  const name = "Piyush";
-  const x = true;
   return (
     <div className="App">
-        <h1>Hello From React</h1>
-        <h2>Hello {name}</h2>
-        <h3>Hello {x?'Yes':'No'}</h3>
         <Header/>
     </div>
   );
