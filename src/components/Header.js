3686d1e6a0da4ff5b0c7e0ba19571f67ccbe0db5
Piyush Khanna <khanna.piyush30@gmail.com>
2021-05-19 19:02:46 +0530

    handling click

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,27 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = (props) => {
+    const onClick = () => {
+        console.log('Header obj');
+    }
     return (
         <header className="header">
             <h1 style={headingStyle}>Hello {props.title} {props.lname}!</h1>
-            <Button color='green' text='Props'/>
+            <Button onClick={onClick} color='green' text='Add' />
         </header>
     )
 }
 Header.defaultProps = {
-    lname:'Khanna'
+    lname: 'Khanna'
 }
 
 Header.propTypes = {
-    title:PropTypes.string,
-    lname:PropTypes.string.isRequired
+    title: PropTypes.string,
+    lname: PropTypes.string.isRequired
 }
 
 const headingStyle = {
-    color:'red', 
-    backgroundColor:'black'
+    color: 'red',
+    backgroundColor: 'black'
 }
 export default Header
