31850eece25c0aabd2b26b14f78e09bd35114702
Piyush Khanna <khanna.piyush30@gmail.com>
2021-05-19 20:42:23 +0530

    delete tasks

--- src/App.js
+++ src/App.js
@@ -23,10 +23,14 @@ function App() {
       }
     ]
   )
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id!=id));
+  }
   return (
     <div className="App">
       <Header title="Piyush" />
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
